6a3d3d9c7d2cd657d87c6569d1e089386ac80dc8
GameurDuXVI <gameur.codeur@gmail.com>
2021-01-29 19:37:25 +0100

    Update BungeeChannelApi

--- src/main/java/eu/builderscoffee/api/Main.java
+++ src/main/java/eu/builderscoffee/api/Main.java
@@ -13,6 +13,7 @@ public class Main extends JavaPlugin {
     @Getter
     private static InventoryManager invManager;
 
+    @Getter
     private static BungeeChannelApi bungeeChannelApi;
 
     @Override
