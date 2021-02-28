f5c2172fc14ab572cac23433da194db0b3d9539d
GameurDuXVI <gameur.codeur@gmail.com>
2021-02-28 21:34:28 +0100

    BungeeCord Support

---
+++ src/main/java/eu/builderscoffee/api/bukkit/Main.java
@@ -0,0 +1,33 @@
+package eu.builderscoffee.api.bukkit;
+
+import eu.builderscoffee.api.bukkit.bungeecord.BungeeChannelApi;
+import eu.builderscoffee.api.bukkit.gui.InventoryManager;
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Getter
+    private static Main instance;
+
+    @Getter
+    private static InventoryManager invManager;
+
+    @Getter
+    private static BungeeChannelApi bungeeChannelApi;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        invManager = new InventoryManager(this);
+        invManager.init();
+
+        bungeeChannelApi = BungeeChannelApi.of(this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
